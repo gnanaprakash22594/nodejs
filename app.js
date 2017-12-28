444f04ac7dc0d9c5c4a65ee9818360f1aeebfa1c
gnanaprakash <gnanaprakash22594@gmail.com>
2017-12-28 13:08:49 +0530

    nodejs sample tasks

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
 console.log('starting app.js')
 
-var connect = require('connect');
 var express = require('express');
 var app = express();
 
-var myLogger = function(req, res, next) {
-    console.log('Logged');
-    next();
-}
-
-var requestTime = function(req, res, next){
-    req.requestTime = (new Date).getTime();
-    next();
-}
-
-
-app.use('/echo/ontro', function (req, res, next) {
-    console.log('Request URL:', req.originalUrl)
+app.use( function( req, res, next ) {
+    console.log( "before" );
     next();
   });
-
-
-app.get('/echo/ontro', function(req, res) {
-    console.log('Hello World');
-});
+app.get( "/hi", function( req, res, next ) {
+    console.log('Request URL:', req.originalUrl);
+    next();       
+  });
+app.use( function( req, res, next ) {
+    console.log( "after" );
+    next();    
+  });
 
 app.listen(3000);
 console.log('Running on port 3000...');
